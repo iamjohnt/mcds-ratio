9ec722356cf657a3b3636f49ec3daf37b3034afa
johnt <tranjohn123@gmail.com>
2021-10-18 09:08:30 -0400

    enabled cross origin to endpoints

--- src/main/java/com/resource/RatioResource.java
+++ src/main/java/com/resource/RatioResource.java
@@ -4,6 +4,7 @@ import com.dao.CaseData;
 import com.dao.HospData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +23,7 @@ public class RatioResource {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+    @CrossOrigin
     @GetMapping("/ratio/case-hosp")
     public List<Double> getCaseHospitalizationRatio() {
 
